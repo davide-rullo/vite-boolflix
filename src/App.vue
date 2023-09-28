747b8997c48ddda38fa7217d35d8d6844be5256b
davide-rullo <davide.rullo01@gmail.com>
2023-09-28 12:08:04 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+
+  name: 'App',
+
+  data() {
+    return {
+
+    }
+  },
+}
+</script>
+
+<template></template>
+
+<style lang="scss">
+@use './assets/scss/partials/variables.scss' as *;
+</style>
