5c687046660fd3b708dcb1a47b3dd7ae4f7f06a2
davide-rullo <davide.rullo01@gmail.com>
2023-09-28 14:38:08 +0200

    add API call and input

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,43 @@
 <script>
+
+import { state } from "./state.js"
+
 export default {
 
   name: 'App',
 
   data() {
     return {
-
+      state
     }
   },
 }
 </script>
 
-<template></template>
+<template>
+  <nav class="navbar">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">
+        <img src="" alt="Logo" width="65" height="24" class="d-inline-block align-text-top">
+
+      </a>
+    </div>
+  </nav>
+  <div class="container">
+    <div class="input-group">
+      <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search"
+        aria-describedby="search-addon" v-model="this.state.movieSelected" />
+      <button type="button" class="btn btn-outline-primary">search</button>
+    </div>
+    <ul>
+      <li>Ciao</li>
+      <li>Ciao</li>
+      <li>Ciao</li>
+      <li>Ciao</li>
+      <li>Ciao</li>
+    </ul>
+  </div>
+</template>
 
 <style lang="scss">
 @use './assets/scss/partials/variables.scss' as *;
