ba6d598096c3b2da751f64b9405abb71f7d90d43
davide-rullo <davide.rullo01@gmail.com>
2023-09-29 16:09:37 +0200

    add css rules

--- src/App.vue
+++ src/App.vue
@@ -36,7 +36,7 @@ export default {
         <div class="input-group p-4">
           <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search"
             aria-describedby="search-addon" v-model="this.state.movieSelected" />
-          <button @click="searchMovies" type="button" class="btn btn-outline-primary">search</button>
+          <button @click="searchMovies" type="button" class="btn btn-outline-light">search</button>
         </div>
       </div>
 
@@ -48,80 +48,105 @@ export default {
     <div class="row-header text-white" v-if="this.search == false">
       <h5>I titoli del momento</h5>
 
-      <div class="row row-cols-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 g-5">
+      <div class="row row-cols-2 row-cols-md-3 row-cols-lg-4 ">
 
-        <div class="col" v-for="movie in state.popularList">
-
-
-          <div class="card">
-            <img v-if="movie.backdrop_path" :src="`https://image.tmdb.org/t/p/w342/` + `${movie.backdrop_path}`" alt="">
-            <img v-else src="./assets/img/noavailable.jpg" height="298.66">
-            <h6>Titolo: </h6>
-            <p>{{ movie.title || movie.name }}</p>
+        <div class="col pb-4" v-for="movie in state.popularList">
 
 
-            <h6> Titolo originale: </h6>
-            <p>{{ movie.original_title || movie.original_name }}</p>
+          <a href="#">
+            <div class="car">
+              <img class="cover" v-if="movie.backdrop_path"
+                :src="`https://image.tmdb.org/t/p/w300/` + `${movie.backdrop_path}`" alt="">
+              <img v-else src="./assets/img/noavailable.jpg" height="298.66">
 
 
-            <h6> Lingua: </h6>
-            <img v-if="movie.original_language == 'en'" class="flag" src="./assets/img/ukflag.png">
-            <img v-else-if="movie.original_language == 'fr'" class="flag" src="./assets/img/france.png">
-            <img v-else-if="movie.original_language == 'it'" class="flag" src="./assets/img/italy.png">
-            <img v-else-if="movie.original_language == 'es'" class="flag" src="./assets/img/spain.png">
+              <div class="shows-info ">
+                <div class="show-title d-flex gap-2">
+                  <h6>Titolo: </h6>
+                  <p> {{ movie.title || movie.name }}</p>
+                </div>
+
+                <div class="original-title d-flex gap-2">
+                  <h6> Titolo originale: </h6>
+                  <p>{{ movie.original_title || movie.original_name }}</p>
+                </div>
 
 
-            <p v-else>{{ movie.original_language }}</p>
+                <div class="lang d-flex gap-2">
+                  <h6> Lingua: </h6>
+                  <img v-if="movie.original_language == 'en'" class="flag" src="./assets/img/ukflag.png">
+                  <img v-else-if="movie.original_language == 'fr'" class="flag" src="./assets/img/france.png">
+                  <img v-else-if="movie.original_language == 'it'" class="flag" src="./assets/img/italy.png">
+                  <img v-else-if="movie.original_language == 'es'" class="flag" src="./assets/img/spain.png">
+                  <p v-else>{{ movie.original_language }}</p>
 
-            <h6> Voto: </h6>
-            <p>{{ Math.ceil((movie.vote_average * 5) / 10) }} </p>
-            <div class="d-flex">
-              <i v-for="i in Math.ceil((movie.vote_average * 5) / 10)" class="fa-solid fa-star"
-                style="color: #ffd500;"></i>
-              <i v-for="i in (5 - Math.ceil((movie.vote_average * 5) / 10))" class="fa-regular fa-star"
-                style="color: #ffd500;"></i>
+                </div>
+
+                <div class="vote d-flex gap-2">
+                  <h6> Voto: </h6>
+
+                  <div class="d-flex">
+                    <i v-for="i in Math.ceil((movie.vote_average * 5) / 10)" class="fa-solid fa-star"
+                      style="color: #ffd500;"></i>
+                    <i v-for="i in (5 - Math.ceil((movie.vote_average * 5) / 10))" class="fa-regular fa-star"
+                      style="color: #ffd500;"></i>
+                  </div>
+                </div>
+              </div>
             </div>
-          </div>
+          </a>
         </div>
       </div>
     </div>
 
+    <!-- END popolari -->
+
+    <!-- Ricerca -->
+    <div class="row row-cols-2 row-cols-md-3 row-cols-lg-4">
+      <div class="col pb-4" v-for="movie in state.moviesList">
+
+        <a href="#">
+          <div class="car">
+            <img class="cover" v-if="movie.backdrop_path"
+              :src="`https://image.tmdb.org/t/p/w300/` + `${movie.backdrop_path}`" alt="">
+            <img v-else src="./assets/img/noavailable.jpg">
 
 
-    <div class="row row-cols-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 g-5">
-      <div class="col" v-for="movie in state.moviesList">
+            <div class="shows-info ">
+              <div class="show-title d-flex gap-2">
+                <h6>Titolo: </h6>
+                <p> {{ movie.title || movie.name }}</p>
+              </div>
+
+              <div class="original-title d-flex gap-2">
+                <h6> Titolo originale: </h6>
+                <p>{{ movie.original_title || movie.original_name }}</p>
+              </div>
 
 
-        <div class="card">
+              <div class="lang d-flex gap-2">
+                <h6> Lingua: </h6>
+                <img v-if="movie.original_language == 'en'" class="flag" src="./assets/img/ukflag.png">
+                <img v-else-if="movie.original_language == 'fr'" class="flag" src="./assets/img/france.png">
+                <img v-else-if="movie.original_language == 'it'" class="flag" src="./assets/img/italy.png">
+                <img v-else-if="movie.original_language == 'es'" class="flag" src="./assets/img/spain.png">
+                <p v-else>{{ movie.original_language }}</p>
 
-          <img v-if="movie.backdrop_path" :src="`https://image.tmdb.org/t/p/w342/` + `${movie.backdrop_path}`" alt="">
-          <img v-else src="./assets/img/noavailable.jpg" height="298.66">
-          <h6>Titolo: </h6>
-          <p>{{ movie.title || movie.name }}</p>
+              </div>
 
+              <div class="vote d-flex gap-2">
+                <h6> Voto: </h6>
 
-          <h6> Titolo originale: </h6>
-          <p>{{ movie.original_title || movie.original_name }}</p>
-
-
-          <h6> Lingua: </h6>
-          <img v-if="movie.original_language == 'en'" class="flag" src="./assets/img/ukflag.png">
-          <img v-else-if="movie.original_language == 'fr'" class="flag" src="./assets/img/france.png">
-          <img v-else-if="movie.original_language == 'it'" class="flag" src="./assets/img/italy.png">
-          <img v-else-if="movie.original_language == 'es'" class="flag" src="./assets/img/spain.png">
-
-
-          <p v-else>{{ movie.original_language }}</p>
-
-          <h6> Voto: </h6>
-          <p>{{ Math.ceil((movie.vote_average * 5) / 10) }} </p>
-          <div class="d-flex">
-            <i v-for="i in Math.ceil((movie.vote_average * 5) / 10)" class="fa-solid fa-star" style="color: #ffd500;"></i>
-            <i v-for="i in (5 - Math.ceil((movie.vote_average * 5) / 10))" class="fa-regular fa-star"
-              style="color: #ffd500;"></i>
+                <div class="d-flex">
+                  <i v-for="i in Math.ceil((movie.vote_average * 5) / 10)" class="fa-solid fa-star"
+                    style="color: #ffd500;"></i>
+                  <i v-for="i in (5 - Math.ceil((movie.vote_average * 5) / 10))" class="fa-regular fa-star"
+                    style="color: #ffd500;"></i>
+                </div>
+              </div>
+            </div>
           </div>
-
-        </div>
+        </a>
 
 
       </div>
@@ -138,6 +163,7 @@ export default {
 
 .navbar {
   background-color: $bf-dark;
+  height: 4rem;
 
   a {
     color: white;
@@ -149,18 +175,47 @@ export default {
 
 }
 
-#app {
-  background-color: $bf-darkContent;
+body {
+  background-color: $bf-dark;
 }
 
+.car {
+  position: relative;
+}
 
+.car img {
+  border-radius: 0.2vw;
 
-.card {
-  height: 400px;
+}
+
+.shows-info {
+  display: none;
+  position: absolute;
+  top: 0;
+  left: 0;
+  line-height: 1.3;
+  padding: 1rem;
+}
+
+.car:hover .shows-info {
+  display: block;
+}
+
+.car:hover .cover {
+  filter: brightness(0.2);
 }
 
 
 .menu-content {
   padding-top: 3rem;
+}
+
+img {
+  max-width: 100%;
+  max-height: 169px;
+}
+
+a {
+  color: white;
 }
 </style>
